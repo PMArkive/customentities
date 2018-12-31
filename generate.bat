8d59eafddc4ec6c651b50674437cc03bdce66e97
Arthurdead <arthurdead@users.noreply.github.com>
2018-12-31 17:41:39 -0200

    update generate

--- generate.bat
+++ generate.bat
@@ -1,9 +1,10 @@
 @echo off
 setlocal
 
-set pydir=C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64
+set pydir=C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64
+set vcxproj_gen2=D:\TheCode2\vcxproj_gen2
 
-"%pydir%\python.exe" "%~dp0\..\..\vcxproj_gen2\vcxproj_gen2\builder.py" --script="%~dp0\projects\extension.py" --sm-path="%~dp0\..\sourcemod" --mm-path="%~dp0\..\metamod" --hl2sdk-root="%~dp0\.."
+"%pydir%\python.exe" "%vcxproj_gen2%\builder.py" --script="%~dp0\projects\extension.py" --sm-path="%~dp0\..\sourcemod" --mm-path="%~dp0\..\metamod" --hl2sdk-root="%~dp0\.."
 
 if errorlevel 1 (
 	pause
